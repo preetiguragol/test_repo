9bcfdd93037215e404983ef1540ff00e781f9fe6
ashish-gopalika <meashishgopalika@gmail.com>
2022-06-28 18:37:48 +0530

    added a new c file

---
+++ new.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    a=10;
+    b=15;
+    c=a+b;
+    printf("%d\n",c);
+    return 0;
+}
