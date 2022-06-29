0e7f0efed4067c0b1bf71dc6b2394b9c7b73b071
preetiguragol <preetiguragol@gmail.com>
2022-06-29 20:21:16 +0530

    add two no.s

---
+++ hello1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers");
+    scanf("%d %d", &a, &b);
+    c=a+b;
+    printf("%d is the sum", c);
+    return 0;
+    
+}
